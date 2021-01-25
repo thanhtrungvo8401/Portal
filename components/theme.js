cf8faadae1bed731713ca08710fcb85e45a4a5e6
trung8401 <thanhtrungvo8401@gmail.com>
2021-01-25 09:15:21 +0700

    01 -- handle change primary color

--- components/theme.js
+++ components/theme.js
@@ -1,10 +1,13 @@
-import { red } from "@material-ui/core/colors";
+import { green, grey, red, yellow } from "@material-ui/core/colors";
 import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
   palette: {
     secondary: {
       main: "#fff",
+    },
+    primary: {
+      main: red[700],
     },
     error: {
       main: red[400],
