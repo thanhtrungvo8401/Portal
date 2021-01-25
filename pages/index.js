77453ca20682233c1c42926feeb107beb0878b1d
trung8401 <thanhtrungvo8401@gmail.com>
2021-01-25 14:36:58 +0700

    01 -- remove unused redux

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,90 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import Link from "next/link";
-import { startClock } from "../redux/actions/actions";
-import Examples from "../components/examples";
+import { Typography } from "@material-ui/core";
 import { withLayout } from "../components/Layouts/Layout";
 
 const Index = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(startClock());
-  }, [dispatch]);
-
   return (
     <>
-      <Examples />
-      <Link href="/show-redux-state">
-        <a>Click to see current Redux State</a>
-      </Link>
-      <Examples />
-      <Link href="/show-redux-state">
-        <a>Click to see current Redux State</a>
-      </Link>
+      <Typography variant="h6" color="error">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+      </Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+        feugiat magna quis purus tempus congue. Morbi eget est orci. Quisque
+        ligula augue, hendrerit in ante a, lobortis dapibus turpis. Cras id
+        elementum ligula. Vestibulum ante ipsum primis in faucibus orci luctus
+        et ultrices posuere cubilia curae; Fusce in elit non lacus tincidunt
+        dapibus. Fusce at imperdiet nisi, quis commodo purus. Nulla vitae
+        euismod neque. Vivamus in venenatis eros, ultricies gravida lectus.
+        Curabitur convallis quam eget est ornare, ut porta purus ultrices. Ut
+        ante lacus, aliquet ac mollis et, vulputate non erat. Cras diam velit,
+        malesuada et venenatis id, tincidunt vel leo. Mauris massa lacus,
+        elementum non sapien eget, fermentum posuere justo. Aliquam erat
+        volutpat. Duis ac sapien nulla. Sed ut tortor eu mauris scelerisque
+        scelerisque. Aenean blandit venenatis nibh. Suspendisse molestie ornare
+        lacus, in dignissim mauris placerat id. Aliquam ac nibh tincidunt mauris
+        volutpat maximus consequat in nulla. Donec nec hendrerit quam. Proin
+        rhoncus urna elit, ut vulputate tellus volutpat a. Integer euismod est
+        bibendum dictum volutpat. Quisque posuere commodo libero nec gravida.
+        Phasellus mi odio, cursus eu tempor ac, faucibus ut mi. Curabitur vitae
+        elementum erat. Donec nec purus enim. Proin venenatis pellentesque
+        velit, ornare porta nulla cursus ut. Nam quis pellentesque lacus. Cras
+        sapien urna, aliquet placerat leo nec, volutpat ullamcorper nisl.
+        Maecenas a fermentum felis. Duis accumsan tortor sit amet lorem
+        tristique feugiat in rutrum sapien. Nam ut nulla sodales ex consequat
+        aliquet. Curabitur luctus rhoncus velit. Mauris finibus iaculis nibh in
+        pellentesque. Vestibulum dapibus congue lectus ac maximus. Donec urna
+        tellus, aliquam ac purus quis, tempus sagittis ipsum. Nulla ornare risus
+        ut bibendum suscipit. Duis interdum tincidunt augue, non feugiat arcu
+        accumsan non. Maecenas interdum tristique tincidunt. Sed quis velit
+        enim. Donec pharetra et arcu in facilisis. Mauris nunc elit, vulputate
+        non placerat vitae, iaculis vel diam. Mauris id lorem semper, rutrum
+        nisl ut, maximus purus. Etiam ut tellus enim. In sit amet mollis dolor.
+        Praesent sollicitudin lacus eu leo accumsan finibus. Ut aliquam lacinia
+        sem eget consectetur. Etiam placerat vulputate dolor, tristique interdum
+        lacus porttitor sed. Sed aliquet lacinia venenatis. Mauris luctus est in
+        fermentum mattis. Suspendisse suscipit imperdiet feugiat. Vivamus felis
+        leo, tincidunt non mattis eget, elementum ac lacus.
+      </Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+        feugiat magna quis purus tempus congue. Morbi eget est orci. Quisque
+        ligula augue, hendrerit in ante a, lobortis dapibus turpis. Cras id
+        elementum ligula. Vestibulum ante ipsum primis in faucibus orci luctus
+        et ultrices posuere cubilia curae; Fusce in elit non lacus tincidunt
+        dapibus. Fusce at imperdiet nisi, quis commodo purus. Nulla vitae
+        euismod neque. Vivamus in venenatis eros, ultricies gravida lectus.
+        Curabitur convallis quam eget est ornare, ut porta purus ultrices. Ut
+        ante lacus, aliquet ac mollis et, vulputate non erat. Cras diam velit,
+        malesuada et venenatis id, tincidunt vel leo. Mauris massa lacus,
+        elementum non sapien eget, fermentum posuere justo. Aliquam erat
+        volutpat. Duis ac sapien nulla. Sed ut tortor eu mauris scelerisque
+        scelerisque. Aenean blandit venenatis nibh. Suspendisse molestie ornare
+        lacus, in dignissim mauris placerat id. Aliquam ac nibh tincidunt mauris
+        volutpat maximus consequat in nulla. Donec nec hendrerit quam. Proin
+        rhoncus urna elit, ut vulputate tellus volutpat a. Integer euismod est
+        bibendum dictum volutpat. Quisque posuere commodo libero nec gravida.
+        Phasellus mi odio, cursus eu tempor ac, faucibus ut mi. Curabitur vitae
+        elementum erat. Donec nec purus enim. Proin venenatis pellentesque
+        velit, ornare porta nulla cursus ut. Nam quis pellentesque lacus. Cras
+        sapien urna, aliquet placerat leo nec, volutpat ullamcorper nisl.
+        Maecenas a fermentum felis. Duis accumsan tortor sit amet lorem
+        tristique feugiat in rutrum sapien. Nam ut nulla sodales ex consequat
+        aliquet. Curabitur luctus rhoncus velit. Mauris finibus iaculis nibh in
+        pellentesque. Vestibulum dapibus congue lectus ac maximus. Donec urna
+        tellus, aliquam ac purus quis, tempus sagittis ipsum. Nulla ornare risus
+        ut bibendum suscipit. Duis interdum tincidunt augue, non feugiat arcu
+        accumsan non. Maecenas interdum tristique tincidunt. Sed quis velit
+        enim. Donec pharetra et arcu in facilisis. Mauris nunc elit, vulputate
+        non placerat vitae, iaculis vel diam. Mauris id lorem semper, rutrum
+        nisl ut, maximus purus. Etiam ut tellus enim. In sit amet mollis dolor.
+        Praesent sollicitudin lacus eu leo accumsan finibus. Ut aliquam lacinia
+        sem eget consectetur. Etiam placerat vulputate dolor, tristique interdum
+        lacus porttitor sed. Sed aliquet lacinia venenatis. Mauris luctus est in
+        fermentum mattis. Suspendisse suscipit imperdiet feugiat. Vivamus felis
+        leo, tincidunt non mattis eget, elementum ac lacus.
+      </Typography>
     </>
   );
 };
 
-export default withLayout(Index, {title: "Dashboard"});
+export default withLayout(Index, { title: "Dashboard" });
