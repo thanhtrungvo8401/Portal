1bdeb39bf48858426f784792a2785ed567096458
trung8401 <thanhtrungvo8401@gmail.com>
2021-01-25 09:19:34 +0700

    01 -- handle change primary color

--- components/theme.js
+++ components/theme.js
@@ -1,4 +1,4 @@
-import { green, grey, red, yellow } from "@material-ui/core/colors";
+import { red } from "@material-ui/core/colors";
 import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
