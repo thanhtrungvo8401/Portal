5d65e20dab4252d7e571cb14f324fe4fb715346b
trung8401 <thanhtrungvo8401@gmail.com>
2021-01-27 13:56:31 +0700

    01 -- handle create study-room

---
+++ pages/study-room/index.js
@@ -0,0 +1,7 @@
+import { withLayout } from "../../components/Layouts/Layout";
+
+function StudyRoom(props) {
+  return <div>STUDY ROOM</div>;
+}
+
+export default withLayout(StudyRoom, { title: "Study Room" });
