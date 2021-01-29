da79d8677e69c3aa5f5f29c572da34c17a03d280
trung8401 <thanhtrungvo8401@gmail.com>
2021-01-29 21:01:53 +0700

    02 -- redefine navbar-size

--- components/Layouts/Navbar.js
+++ components/Layouts/Navbar.js
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
-import { AppBar, Button, IconButton, Toolbar } from "@material-ui/core";
+import {
+  AppBar,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import { AccountCircle } from "@material-ui/icons";
 import {
@@ -55,70 +61,72 @@ function Navbar(props) {
     <React.Fragment>
       <HideOnScroll {...props}>
         <AppBar color="primary">
-          <Toolbar>
-            <MyLink variant="h6" url="/" className={classes.title}>
-              Neko-kun
-            </MyLink>
+          <Container>
+            <Toolbar>
+              <MyLink variant="h6" url="/" className={classes.title}>
+                Neko-kun
+              </MyLink>
 
-            <div className={classes.sectionDesktop}>
-              <MyLink
-                variant="h6"
-                className={classes.navItem}
-                url="/top-student"
-              >
-                Top Student
-              </MyLink>
-              <MyLink
-                variant="h6"
-                className={classes.navItem}
-                url="/top-student"
-              >
-                News
-              </MyLink>
-              <MyLink
-                variant="h6"
-                className={classes.navItem}
-                url="/top-student"
-              >
-                About Neko
-              </MyLink>
-            </div>
-            <div className={classes.sectionMobile}>
-              <IconButton
-                aria-label="show more"
-                aria-controls={mobileMenuId}
-                aria-haspopup="true"
-                onClick={handleMobileMenuOpen}
-                color="secondary"
-              >
-                <MenuIcon />
-              </IconButton>
-            </div>
-            <div className={classes.grow} />
-            <div className={classes.roomEnter}>
-              <MyLink variant="h6" url="/study-room">
-                <Button
-                  color="secondary"
-                  variant="outlined"
-                  className={styles.flashEffect}
+              <div className={classes.sectionDesktop}>
+                <MyLink
+                  variant="h6"
+                  className={classes.navItem}
+                  url="/top-student"
                 >
-                  STUDY NOW
-                </Button>
-              </MyLink>
-            </div>
-            <div>
-              <IconButton
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={profileId}
-                aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="secondary"
-              >
-                <AccountCircle />
-              </IconButton>
-            </div>
-          </Toolbar>
+                  Top Student
+                </MyLink>
+                <MyLink
+                  variant="h6"
+                  className={classes.navItem}
+                  url="/top-student"
+                >
+                  News
+                </MyLink>
+                <MyLink
+                  variant="h6"
+                  className={classes.navItem}
+                  url="/top-student"
+                >
+                  About Neko
+                </MyLink>
+              </div>
+              <div className={classes.sectionMobile}>
+                <IconButton
+                  aria-label="show more"
+                  aria-controls={mobileMenuId}
+                  aria-haspopup="true"
+                  onClick={handleMobileMenuOpen}
+                  color="secondary"
+                >
+                  <MenuIcon />
+                </IconButton>
+              </div>
+              <div className={classes.grow} />
+              <div className={classes.roomEnter}>
+                <MyLink variant="h6" url="/study-room">
+                  <Button
+                    color="secondary"
+                    variant="outlined"
+                    className={styles.flashEffect}
+                  >
+                    STUDY NOW
+                  </Button>
+                </MyLink>
+              </div>
+              <div>
+                <IconButton
+                  edge="end"
+                  aria-label="account of current user"
+                  aria-controls={profileId}
+                  aria-haspopup="true"
+                  onClick={handleProfileMenuOpen}
+                  color="secondary"
+                >
+                  <AccountCircle />
+                </IconButton>
+              </div>
+            </Toolbar>
+          </Container>
         </AppBar>
       </HideOnScroll>
       {renderMobileMenu}
