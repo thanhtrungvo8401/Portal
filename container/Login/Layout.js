8cbfbcc97a4777016bcbd1943eee599813190df6
trung8401 <thanhtrungvo8401@gmail.com>
2021-02-19 11:07:41 +0700

    03 -- handle finish layout for login page

--- container/Login/Layout.js
+++ container/Login/Layout.js
@@ -5,14 +5,13 @@ import {
   Container,
   Dialog,
   DialogContent,
-  DialogTitle,
   FormControlLabel,
   Grid,
   makeStyles,
-  TextField,
   Typography,
 } from "@material-ui/core";
 import VpnKeyOutlinedIcon from "@material-ui/icons/VpnKeyOutlined";
+import InputGroup from "../../components/InputGroup";
 import MyLink from "../../components/MyLink";
 const useStyles = makeStyles((theme) => ({
   dialog: {
@@ -32,9 +31,13 @@ const useStyles = makeStyles((theme) => ({
   container: {
     padding: 0,
   },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+  rememberMe: {
+    display: "flex",
+    justifyContent: "flex-start",
+    width: "100%",
+    "& span.MuiTypography-root.MuiFormControlLabel-label.MuiTypography-body1": {
+      textAlign: "left!important"
+    },
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
@@ -47,8 +50,18 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.secondary.main,
   },
 }));
+const inputFields = ["email", "password"];
+const inputTypes = {
+  email: "input",
+  password: "input",
+};
+const inputLabels = {
+  email: "Email Address",
+  password: "Password",
+};
+const inputRequired = ["email", "password"];
 function LoginLayout(props) {
-  const { showLogin } = props;
+  const { showLogin, user, ERROR } = props;
   const classes = useStyles();
   return (
     <Dialog
@@ -64,44 +77,28 @@ function LoginLayout(props) {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <form className={classes.form}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-          </form>
-
+          <InputGroup
+            ERROR={ERROR}
+            inputFields={inputFields}
+            inputTypes={inputTypes}
+            inputLabels={inputLabels}
+            inputRequired={inputRequired}
+            object={user}
+            handleOnChange={props.handleOnChange}
+          />
+          <FormControlLabel
+            className={classes.rememberMe}
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            className={classes.submit}
+            type="btn"
+            variant="contained"
+            color="primary"
+          >
+            Log In
+          </Button>
           <Grid container>
             <Grid item xs>
               <MyLink
--- container/Login/index.js
+++ container/Login/index.js
@@ -1,4 +1,5 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { actionSetUser } from "../../redux/actions/userActions";
 import { closeLoginForm } from "../../utils/Helper";
 import LoginLayout from "./Layout";
 
@@ -6,13 +7,27 @@ function Login(props) {
   const showLogin = useSelector(
     (state) => state.login && state.login.showLogin
   );
-
+  const user = useSelector((state) => state.user);
+  const ERROR = useSelector((state) => state.error);
+  const dispatch = useDispatch();
   // UI INTERACT:
   const handleCloseLogin = () => {
     closeLoginForm();
   };
+  const handleOnChange = (e) => {
+    const target = e.target;
+    const { name, value } = target;
+    const newUser = { ...user, [name]: value };
+    dispatch(actionSetUser(newUser));
+  };
   return (
-    <LoginLayout handleCloseLogin={handleCloseLogin} showLogin={showLogin} />
+    <LoginLayout
+      user={user}
+      ERROR={ERROR}
+      handleOnChange={handleOnChange}
+      handleCloseLogin={handleCloseLogin}
+      showLogin={showLogin}
+    />
   );
 }
 
