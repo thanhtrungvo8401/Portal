8bedc7e54d705e9d48b9424a9ed77bfd0dfcdb7d
trung8401 <thanhtrungvo8401@gmail.com>
2021-02-22 10:55:23 +0700

    05 -- redefine color for avartar

--- container/SignUp/Layout.js
+++ container/SignUp/Layout.js
@@ -10,7 +10,7 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.primary.main,
   },
 }));
 
