778758408e28b06a38f11fd048fcfb526bd14834
trung8401 <thanhtrungvo8401@gmail.com>
2021-02-22 14:26:07 +0700

    05 redefined set-vocas component

--- components/SetVoca.js
+++ components/SetVoca.js
@@ -2,7 +2,7 @@ import PropType from "prop-types";
 import { constantApp } from "../utils/Constant";
 
 import { makeStyles } from "@material-ui/core/styles";
-import { Card, CardContent, Typography } from "@material-ui/core";
+import { Button, Card, CardContent, Typography } from "@material-ui/core";
 import { formatDate } from "../utils/DateHelper";
 import { appUrl } from "../utils/URL";
 import { navigate } from "../utils/Helper";
@@ -10,27 +10,17 @@ import { navigate } from "../utils/Helper";
 const useStyles = makeStyles((theme) => {
   return {
     setItem: {
-      border: `${theme.palette.secondary.main} 1px solid`,
-      marginBottom: theme.spacing(2),
-      "& .MuiCardContent-root": {
+      width: "100%",
+      "& .MuiButton-label": {
         justifyContent: "space-between",
         display: "flex",
         flexWrap: "wrap",
-        padding: "8px 16px",
-        cursor: "pointer",
-        alignItems: "center",
-        backgroundColor: theme.palette.primary.light,
-        transition: theme.transitions.create("all", {
-          duration: theme.transitions.duration.short,
-        }),
-        "&:hover": {
-          backgroundColor: theme.palette.primary.dark,
-        },
       },
     },
     setName: {
       display: "block",
       width: "100%",
+      textAlign: "left",
       textTransform: "uppercase",
       [theme.breakpoints.up("sm")]: {
         display: "inline",
@@ -47,27 +37,26 @@ function SetVoca(props) {
     navigate(appUrl.setVocaDetail("1234"));
   };
   return (
-    <Card
+    <Button
       variant="outlined"
+      color="primary"
       className={classes.setItem}
       onClick={handleGoToSetVocas}
     >
-      <CardContent>
-        <Typography
-          className={classes.setName}
-          color="secondary"
-          variant="subtitle1"
-        >
-          {name}
-        </Typography>
-        <Typography color="secondary" variant="subtitle2">
-          {`${number}/${constantApp.setVocaLimit}`}
-        </Typography>
-        <Typography color="secondary" variant="caption">
-          {formatDate(time)}
-        </Typography>
-      </CardContent>
-    </Card>
+      <Typography
+        className={classes.setName}
+        color="textPrimary"
+        variant="subtitle1"
+      >
+        {name}
+      </Typography>
+      <Typography color="textPrimary" variant="subtitle2">
+        {`${number}/${constantApp.setVocaLimit}`}
+      </Typography>
+      <Typography color="textPrimary" variant="caption">
+        {formatDate(time)}
+      </Typography>
+    </Button>
   );
 }
 
