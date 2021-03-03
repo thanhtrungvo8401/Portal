9737d39215473ead004495eafbe700e7bbc61f68
trung8401 <thanhtrungvo8401@gmail.com>
2021-03-03 21:52:56 +0700

    05 -- create layout for actions btn of SetVoca

--- components/SetVoca.js
+++ components/SetVoca.js
@@ -2,13 +2,27 @@ import PropType from "prop-types";
 import { constantApp } from "../utils/Constant";
 
 import { makeStyles } from "@material-ui/core/styles";
-import { Button, Typography } from "@material-ui/core";
+import {
+  Button,
+  ButtonGroup,
+  MenuItem,
+  Typography,
+  Menu,
+} from "@material-ui/core";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import DeleteOutlinedIcon from "@material-ui/icons/DeleteOutlined";
 import { formatDate } from "../utils/DateHelper";
 import { appUrl } from "../utils/APP_URL";
 import { navigate } from "../utils/Helper";
+import React, { useState } from "react";
 
 const useStyles = makeStyles((theme) => {
   return {
+    btnGroup: {
+      width: "100%",
+      marginBottom: theme.spacing(1),
+    },
     setItem: {
       width: "100%",
       "& .MuiButton-label": {
@@ -16,7 +30,6 @@ const useStyles = makeStyles((theme) => {
         display: "flex",
         flexWrap: "wrap",
       },
-      marginBottom: theme.spacing(1),
     },
     setName: {
       display: "block",
@@ -28,36 +41,79 @@ const useStyles = makeStyles((theme) => {
         width: "auto",
       },
     },
+    marginLeft: {
+      marginLeft: theme.spacing(1),
+    },
   };
 });
 function SetVoca(props) {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
   const { name, number, time } = props;
   // UI INTERACT:
   const handleGoToSetVocas = (setVoca) => {
     navigate(appUrl.setVocaDetail("1234"));
   };
+  const handleOpenAction = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseAction = (event) => {
+    setAnchorEl(null);
+  };
+  const handleEditBtnClick = (event) => {
+    handleCloseAction();
+  };
+  const handleRemoveBtnClick = (event) => {
+    handleCloseAction();
+  };
   return (
-    <Button
-      variant="outlined"
-      color="primary"
-      className={classes.setItem}
-      onClick={handleGoToSetVocas}
-    >
-      <Typography
-        className={classes.setName}
-        color="textPrimary"
-        variant="subtitle1"
+    <React.Fragment>
+      <ButtonGroup className={classes.btnGroup}>
+        <Button
+          variant="outlined"
+          color="primary"
+          className={classes.setItem}
+          onClick={handleGoToSetVocas}
+        >
+          <Typography
+            className={classes.setName}
+            color="textPrimary"
+            variant="subtitle1"
+          >
+            {name}
+          </Typography>
+          <Typography color="textPrimary" variant="subtitle2">
+            {`${number}/${constantApp.setVocaLimit}`}
+          </Typography>
+          <Typography color="textPrimary" variant="caption">
+            {formatDate(time)}
+          </Typography>
+        </Button>
+        <Button variant="outlined" size="small" onClick={handleOpenAction}>
+          <ArrowDropDownIcon />
+        </Button>
+      </ButtonGroup>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleCloseAction}
       >
-        {name}
-      </Typography>
-      <Typography color="textPrimary" variant="subtitle2">
-        {`${number}/${constantApp.setVocaLimit}`}
-      </Typography>
-      <Typography color="textPrimary" variant="caption">
-        {formatDate(time)}
-      </Typography>
-    </Button>
+        <MenuItem key={"edit"} onClick={handleEditBtnClick}>
+          <EditOutlinedIcon />
+          <Typography className={classes.marginLeft} variant="caption">
+            Edit
+          </Typography>
+        </MenuItem>
+        <MenuItem key={"delete"} onClick={handleRemoveBtnClick}>
+          <DeleteOutlinedIcon color="error" />
+          <Typography className={classes.marginLeft} variant="caption">
+            Remove
+          </Typography>
+        </MenuItem>
+      </Menu>
+    </React.Fragment>
   );
 }
 
