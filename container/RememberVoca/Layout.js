340a222c2b52bdf1a5e49ee080ef40f09c851dca
trung8401 <thanhtrungvo8401@gmail.com>
2021-03-27 20:18:37 +0700

    007 -- create action-group for create remember-groups

--- container/RememberVoca/Layout.js
+++ container/RememberVoca/Layout.js
@@ -1,13 +1,26 @@
-import { Typography } from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
+import { useState } from "react";
+import ActionGroup from "../../components/ActionGroup";
 import MultiTabStudy from "../../components/MultiTabStudy/MultiTabStudy";
 import PageTitle from "../../components/PageComponent/PageTitle";
 import ParagraphBody from "../../components/ParagraphBody";
 import ParagraphTitle from "../../components/ParagraphTitle";
 
 function RememberVocasLayout(props) {
+  const listRememberGroups = [];
+
+  const [isShowCreateForm, setCreateForm] = useState(false);
+  const isValidSubmit = false;
+  const handleShowCreate = () => {
+    setCreateForm(true);
+  };
+  const handleCloseCreate = () => {
+    setCreateForm(false);
+  };
   return (
     <div className="remember-vocas-layout">
       <MultiTabStudy />
+      {/* intro */}
       <PageTitle>Let's learn vocabularies</PageTitle>
       <ParagraphTitle>Instruction</ParagraphTitle>
       <ParagraphBody>
@@ -15,10 +28,43 @@ function RememberVocasLayout(props) {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
         </Typography>
       </ParagraphBody>
+      {/* main */}
       <ParagraphTitle>Learning vocabularies group</ParagraphTitle>
       <ParagraphBody>
-        STUDY
+        {!isShowCreateForm && !listRememberGroups.length && (
+          <Typography>
+            There are no remembers-group, Click into the following (+) Icon to
+            create a new one!
+          </Typography>
+        )}
       </ParagraphBody>
+      {/* action */}
+      <ActionGroup>
+        {isShowCreateForm && (
+          <Button variant="contained" onClick={handleCloseCreate}>
+            Cancle
+          </Button>
+        )}
+        {isShowCreateForm && (
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={props.handleOnSubmit}
+            disabled={!isValidSubmit}
+          >
+            Save
+          </Button>
+        )}
+        {!isShowCreateForm && (
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={handleShowCreate}
+          >
+            Create (+)
+          </Button>
+        )}
+      </ActionGroup>
     </div>
   );
 }
