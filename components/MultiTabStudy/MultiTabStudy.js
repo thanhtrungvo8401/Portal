532ca01d4d9d0c204526b6fc0c004735ed95c891
trung8401 <thanhtrungvo8401@gmail.com>
2021-03-27 15:24:11 +0700

    07 -- handle custom for multi-tab

--- components/MultiTabStudy/MultiTabStudy.js
+++ components/MultiTabStudy/MultiTabStudy.js
@@ -1,16 +1,27 @@
-import { makeStyles, Paper, Tab, Tabs } from "@material-ui/core";
+import { Paper, Tab, Tabs } from "@material-ui/core";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import CastForEducationIcon from "@material-ui/icons/CastForEducation";
 import FiberNewIcon from "@material-ui/icons/FiberNew";
 import MenuBookIcon from "@material-ui/icons/MenuBook";
-import React, { useEffect, useRef, useState } from "react";
-import { isServer } from "../../utils/Helper";
+import React, { useEffect, useState } from "react";
+import { appUrl } from "../../utils/APP_URL";
+import { isServer, navigate } from "../../utils/Helper";
 
-const useStyles = makeStyles((theme) => ({}));
+// const useStyles = makeStyles((theme) => ({}));
 
 const listTabItem = [
-  { Icon: MenuBookIcon, label: "Nhớ từ" },
-  { Icon: FiberNewIcon, label: "Tạo thêm từ" },
-  { Icon: CastForEducationIcon, label: "Kiểm tra kiến thức" },
+  {
+    Icon: ArrowBackIcon,
+    label: "Quay lại phòng học",
+    url: appUrl.studyRoom(),
+  },
+  { Icon: MenuBookIcon, label: "Nhớ từ", url: appUrl.rememberVoca() },
+  { Icon: FiberNewIcon, label: "Tạo thêm từ", url: appUrl.myVoca() },
+  {
+    Icon: CastForEducationIcon,
+    label: "Kiểm tra kiến thức",
+    url: appUrl.testVoca(),
+  },
 ];
 
 export default function MultiTabStudy() {
@@ -18,21 +29,30 @@ export default function MultiTabStudy() {
   const [isDestop, setIsDesktop] = useState(
     !isServer && window.innerWidth > 600
   );
+  const [activeTab, setActiveTab] = useState(0);
   const getCurrentActiveTab = () => {
-    const location = window.location.pathname;
-    console.log(location);
+    const tabUrl = window.location.pathname;
+    const studyTabArray = [
+      appUrl.studyRoom(),
+      appUrl.rememberVoca(),
+      appUrl.myVoca(),
+      appUrl.testVoca(),
+    ];
+    setActiveTab(studyTabArray.indexOf(tabUrl));
+  };
+  const listenerResizeScreen = () => {
+    if (window.innerWidth < 600) {
+      setIsDesktop(false);
+    }
+    if (window.innerWidth > 600) {
+      setIsDesktop(true);
+    }
   };
   useEffect(() => {
     getCurrentActiveTab();
-    window.addEventListener("resize", () => {
-      if (window.innerWidth < 600) {
-        setIsDesktop(false);
-      }
-      if (window.innerWidth > 600) {
-        setIsDesktop(true);
-      }
-    });
+    window.addEventListener("resize", listenerResizeScreen);
   }, []);
+  // console.log(activeTab === 0 && index === 0);
   return (
     <React.Fragment>
       <Paper elevation={0}>
@@ -41,11 +61,20 @@ export default function MultiTabStudy() {
           scrollButtons="on"
           indicatorColor="secondary"
           textColor="secondary"
-          value={1}
+          value={activeTab}
         >
-          {listTabItem.map(({ Icon, label }, index) => {
+          {listTabItem.map(({ Icon, label, url }, index) => {
             return (
-              <Tab key={index} label={isDestop ? label : ""} icon={<Icon />} />
+              <Tab
+                key={index}
+                label={isDestop ? label : ""}
+                icon={<Icon />}
+                hidden={true}
+                style={{
+                  display: activeTab === 0 && index === 0 ? "none" : "block",
+                }}
+                onClick={() => navigate(url)}
+              />
             );
           })}
         </Tabs>
--- utils/APP_URL.js
+++ utils/APP_URL.js
@@ -1,7 +1,7 @@
 export const appUrl = {
   studyRoom: () => "/study-room",
   rememberVoca: () => `/study-room/remember-vocas`,
-  createVoca: () => `/study-room/my-vocas`,
+  myVoca: () => `/study-room/my-vocas`,
   setVocaDetail: (setId) => `/study-room/my-vocas/${setId}`,
   testVoca: () => `/study-room/test-vocas`,
   dashboard: () => "/",
