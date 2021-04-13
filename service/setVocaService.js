d99542c7adc4465db9b0d20ea87781894b6c8edd
trung8401 <thanhtrungvo8401@gmail.com>
2021-04-13 21:57:20 +0700

    007 -- update set-vocas-service

--- service/setVocaService.js
+++ service/setVocaService.js
@@ -2,25 +2,35 @@ import { API } from "../api/Api";
 import { toast } from "../components/Toast";
 import { actionSetError } from "../redux/actions/errorActions";
 import {
-  actionAddGroupVocasItem,
-  actionRemoveGroupVocasItem,
-  actionResetListEding,
-  actionSetGroupVocasList,
-  actionSetValueForSetVocaEditing,
-  actionSetvocaObject,
-  actionUpdateSetVocaAfterUpdate,
+  actionAddSetVocaToList,
+  actionRemoveSetVocaFromList,
+  actionSetIsShowSetVocaModal,
+  actionSet_SetVoca_List,
+  actionUpdateSetVocaInList,
 } from "../redux/actions/setVocasActions";
 import { enpoint_setVoca } from "../utils/API_URL";
-import { appUrl } from "../utils/APP_URL";
 import { codeToMessages, constCODE } from "../utils/CodeToMessages";
-import { handleErrorAPI, navigate } from "../utils/Helper";
+import { handleErrorAPI } from "../utils/Helper";
+
+export const serviceGetSetVocas = (authId) => {
+  return (dispatch) => {
+    API.get(enpoint_setVoca.getSetVocasByAuthId(authId))
+      .then((res) => {
+        dispatch(actionSet_SetVoca_List(res.data));
+      })
+      .catch((err) => {
+        const object = handleErrorAPI(err, "toast");
+        dispatch(actionSetError(object.errorCodesObject));
+      });
+  };
+};
 
 export const serviceCreateSetVoca = (setVoca) => {
   return (dispatch) => {
     API.post(enpoint_setVoca.createSetVocas(), setVoca)
       .then((res) => {
-        const newSetVoca = res.data;
-        dispatch(actionAddGroupVocasItem(newSetVoca));
+        dispatch(actionAddSetVocaToList(res.data));
+        dispatch(actionSetIsShowSetVocaModal(false));
         toast.success(codeToMessages(constCODE.CREATE_SET_VOCAS_SUCCESS));
       })
       .catch((err) => {
@@ -34,10 +44,8 @@ export const serviceUpdateSetVoca = (setVoca) => {
   return (dispatch) => {
     API.put(enpoint_setVoca.updateSetVocas(setVoca.id), setVoca)
       .then((res) => {
-        const voca = res.data;
-        dispatch(actionUpdateSetVocaAfterUpdate(voca));
-        dispatch(actionResetListEding());
-        dispatch(actionSetValueForSetVocaEditing({}));
+        dispatch(actionUpdateSetVocaInList(res.data));
+        dispatch(actionSetIsShowSetVocaModal(false));
         toast.success(codeToMessages(constCODE.UPDATE_SET_VOCAS_SUCCESS));
       })
       .catch((err) => {
@@ -47,44 +55,11 @@ export const serviceUpdateSetVoca = (setVoca) => {
   };
 };
 
-export const serviceGetSetVocas = (authId) => {
-  return (dispatch) => {
-    API.get(enpoint_setVoca.getSetVocasByAuthId(authId))
-      .then((res) => {
-        const listSetVocas = res.data;
-        dispatch(actionSetGroupVocasList(listSetVocas));
-      })
-      .catch((err) => {
-        const object = handleErrorAPI(err, "toast");
-        dispatch(actionSetError(object.errorCodesObject));
-      });
-  };
-};
-
-export const serviceGetSetVocaDetail = (id) => {
-  return (dispatch) => {
-    API.get(enpoint_setVoca.getSetVocaDetailById(id))
-      .then((res) => {
-        const setVoca = res.data;
-        dispatch(actionSetvocaObject(setVoca));
-      })
-      .catch((err) => {
-        const object = handleErrorAPI(err, "toast");
-        const status = object.status;
-        if (status === 400) {
-          navigate(appUrl.studyRoom());
-        }
-        dispatch(actionSetError(object.errorCodesObject));
-      });
-  };
-};
-
 export const serviceDeleteSetVocas = (id) => {
   return (dispatch) => {
     API.delete(enpoint_setVoca.deleteById(id))
       .then((res) => {
-        const deletedItem = res.data;
-        dispatch(actionRemoveGroupVocasItem(deletedItem));
+        dispatch(actionRemoveSetVocaFromList(res.data));
         toast.success(codeToMessages(constCODE.DELETE_SET_VOCAS_SUCCESS));
       })
       .catch((err) => {
