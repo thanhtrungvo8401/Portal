26841017fee77cb4e6211f09a5d3eb722c72dea5
trung8401 <thanhtrungvo8401@gmail.com>
2021-04-18 11:50:25 +0700

    007 -- creat component for display voca-list

--- container/RememberVoca/Step3.js
+++ container/RememberVoca/Step3.js
@@ -1,7 +1,4 @@
 import {
-  List,
-  ListItem,
-  ListItemText,
   makeStyles,
   TextField,
   Typography,
@@ -10,6 +7,7 @@ import { Autocomplete } from "@material-ui/lab";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import theme from "../../components/theme";
+import VocaList from "../../components/VocaGroup/VocaList";
 import { serviceGetCenterSetVocas } from "../../service/setVocaService";
 import { serviceFetVocaBySetId } from "../../service/vocaService";
 import {
@@ -21,14 +19,10 @@ import { localStorageHelper } from "../../utils/storageHelper";
 
 const step3Styles = makeStyles((theme) => ({
   root: {},
-  vocasCover: {
-    maxHeight: "18rem",
-    overflowY: "scroll",
-  },
 }));
 
 export default function Step3({ object, actionUpdate }) {
-  const { type, level } = object;
+  const { type } = object;
   const isDefaultCenter = type === CREATE_REMEMBER_TYPE.TYPE_DEFAULT_CENTER_SET;
   const classes = step3Styles();
   return (
@@ -44,7 +38,6 @@ export default function Step3({ object, actionUpdate }) {
 }
 
 function VocasFromYourOwnSet({ object, actionUpdate }) {
-  const classes = step2Styles();
   const dispatch = useDispatch();
   const { setVoca } = object;
   const vocas = useSelector((state) => state.vocas).list;
@@ -67,27 +60,7 @@ function VocasFromYourOwnSet({ object, actionUpdate }) {
       >
         These vocabularies will be added into your remember group
       </Typography>
-      <div className={classes.vocasCover}>
-        <List>
-          {vocas.map((el, index) => (
-            <ListItem
-              key={el.id}
-              button
-              divider={index !== vocas.length - 1}
-              style={{ display: "flex", justifyContent: "space-between" }}
-            >
-              <ListItemText
-                secondary={el.voca}
-                style={{ width: "calc(50% - 0.5rem)", marginRight: "1rem" }}
-              />
-              <ListItemText
-                secondary={el.meaning}
-                style={{ width: "calc(50% - 0.5rem)" }}
-              />
-            </ListItem>
-          ))}
-        </List>
-      </div>
+      <VocaList vocas={vocas} />
     </React.Fragment>
   );
 }
