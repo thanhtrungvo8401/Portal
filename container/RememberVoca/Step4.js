4dced369ec9776d1becdf2e4f3c289e95a06dfb0
trung8401 <thanhtrungvo8401@gmail.com>
2021-04-18 10:38:15 +0700

    007 -- handle create step-4 for create remembergroup from default-center

--- container/RememberVoca/Step4.js
+++ container/RememberVoca/Step4.js
@@ -1,44 +1,105 @@
-import {
-  Checkbox,
-  FormControlLabel,
-  makeStyles,
-  Typography,
-} from "@material-ui/core";
-import theme from "../../components/theme";
+import { Button, ButtonGroup, makeStyles } from "@material-ui/core";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import VocaGroup from "../../components/VocaGroup/VocaGroup";
+import { serviceFetVocaBySetId } from "../../service/vocaService";
 
 const step4Styles = makeStyles((theme) => ({
-  root: {},
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  button: {
+    margin: theme.spacing(0.5, 0),
+  },
 }));
 
+const not = (a, b) => a.filter((el) => b.indexOf(el) === -1);
+const intersection = (a, b) => a.filter((el) => b.indexOf(el) !== -1);
+
 export default function Step4({ object, actionUpdate }) {
-  const { setVoca, isRemoveStudiedVoca } = object;
   const classes = step4Styles();
+  const dispatch = useDispatch();
+  const { setVoca } = object;
+  const appVocas = useSelector((state) => state.vocas.list);
+  const ids = appVocas.map((voca) => voca.id);
+
+  const [checked, setChecked] = React.useState([]);
+  const [resources, setResources] = React.useState(ids.slice(7));
+  const [remembers, setRemembers] = React.useState(ids.slice(0, 7));
+
+  const resourcesChecked = intersection(checked, resources);
+  const remembersChecked = intersection(checked, remembers);
+  const handleCheckedRemembers = () => {
+    setRemembers(remembers.concat(resourcesChecked));
+    setResources(not(resources, resourcesChecked));
+    setChecked(not(checked, resourcesChecked));
+  };
+  const handleCheckedResources = () => {
+    setResources(resources.concat(remembersChecked));
+    setRemembers(not(remembers, remembersChecked));
+    setChecked(not(checked, resourcesChecked));
+  };
+  const handleToggle = (value) => () => {
+    setChecked(
+      !checked.includes(value)
+        ? [...checked, value]
+        : checked.filter((el) => el !== value)
+    );
+  };
+  const getDetail = (id) => {
+    const voca = appVocas.find((voca) => voca.id === id);
+    return { voca: voca.voca, meaning: voca.meaning };
+  };
+
+  React.useEffect(() => {
+    dispatch(serviceFetVocaBySetId(setVoca.id));
+  }, []);
+  React.useEffect(() => {
+    actionUpdate({
+      ...object,
+      vocas: appVocas.filter((el) => remembers.includes(el.id)),
+    });
+  }, [remembers]);
+
   return (
     <div className={classes.root}>
-      <Typography
-        variant="subtitle2"
-        style={{ marginBottom: theme.spacing(1) }}
-      >
-        Your remember-group will be created from{" "}
-        <span style={{ fontStyle: "italic" }}>{setVoca.setName}</span>
-      </Typography>
-      <FormControlLabel
-        control={
-          <Checkbox
-            color="primary"
-            name="isRemoveStudiedVoca"
-            checked={isRemoveStudiedVoca}
-            onChange={(e) =>
-              actionUpdate({ ...object, isRemoveStudiedVoca: e.target.checked })
-            }
-          />
-        }
-        label="Remove studied vocabularies"
+      <VocaGroup
+        title="Your remember group"
+        getDetail={getDetail}
+        activeList={checked}
+        handleToggle={handleToggle}
+        items={remembers}
+        isActive={true}
       />
-      <Typography hidden={isRemoveStudiedVoca} variant="caption" color="error">
-        You should use click into this option to avoid repeating the word that
-        you have already remembered!!!
-      </Typography>
+      <ButtonGroup disableElevation color="primary" variant="contained">
+        <Button
+          size="small"
+          className={classes.button}
+          onClick={handleCheckedRemembers}
+          disabled={!resourcesChecked.length}
+          aria-label="move selected right"
+        >
+          <span style={{ transform: "rotate(-90deg)" }}>&gt;</span>
+        </Button>
+        <Button
+          size="small"
+          className={classes.button}
+          onClick={handleCheckedResources}
+          disabled={!remembersChecked.length}
+          aria-label="move selected left"
+        >
+          <span style={{ transform: "rotate(-90deg)" }}>&lt;</span>
+        </Button>
+      </ButtonGroup>
+      <VocaGroup
+        title={setVoca.setName}
+        activeList={checked}
+        handleToggle={handleToggle}
+        items={resources}
+        getDetail={getDetail}
+      />
     </div>
   );
 }
