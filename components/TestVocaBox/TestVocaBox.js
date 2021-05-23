d092c27104912c76ba35a3b222afdfb87be1804a
trung8401 <thanhtrungvo8401@gmail.com>
2021-05-23 13:05:56 +0700

    007 -- finish create hop-ki-uc ui

--- components/TestVocaBox/TestVocaBox.js
+++ components/TestVocaBox/TestVocaBox.js
@@ -1,16 +1,13 @@
 import { Avatar, Box, Button, Card, Divider, List, ListItem, ListItemAvatar, ListItemText, makeStyles, Typography } from "@material-ui/core";
-import { CSSTransition } from "react-transition-group";
 import { theme } from "../../components/theme";
-import FaceIcon from '@material-ui/icons/Face';
 import EditIcon from '@material-ui/icons/Edit';
 import { constantApp } from "../../utils/Constant";
 import React from "react";
 import TestVocaModal from "../TestVocaModal/TestVocaModal";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { actionSetIsShowModal } from "../../redux/actions/testVocaActions";
 
 const destopWidth = "10rem";
-const animationDuration = constantApp.animationDuration;
 
 const useStyles = makeStyles(theme => ({
   Card: {
@@ -93,6 +90,8 @@ const useStyles = makeStyles(theme => ({
 export default function TestVocaBox({ }) {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const { number, resources } = useSelector(state => state.testVoca);
+  let hideDivider = null;
   return <React.Fragment>
     <Card className={classes.Card} >
       <Box className={classes.LeftPart} >
@@ -116,44 +115,43 @@ export default function TestVocaBox({ }) {
       </Typography>
         <Box className={classes.MainContentBox} >
           <List className="list">
+            {/* SO LUONG */}
+            <Typography variant="h6" style={{ textAlign: "center" }} >Số lượng</Typography>
             <ListItem className="list-item" >
               <ListItemAvatar>
-                <Avatar className="avatar" >50</Avatar>
+                <Avatar className="avatar" >数</Avatar>
               </ListItemAvatar>
               <ListItemText>
-                Số lượng từ vựng bạn sẽ học từ những bài bên dưới
-            </ListItemText>
-            </ListItem>
-
-            <Divider className='divider' />
-            <ListItem className="list-item" >
-              <ListItemAvatar>
-                <Avatar className="avatar" >N5</Avatar>
-              </ListItemAvatar>
-              <ListItemText>
-                {[26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40].map(el => {
-                  return <Typography key={el}
-                    className="lesson-name">
-                    {el}
-                  </Typography>
-                })}
+                <Typography style={{ display: "inline-block" }} variant="h6" color="primary" >
+                  {number}
+                </Typography>
               </ListItemText>
             </ListItem>
-
-            <Divider className='divider' />
-            <ListItem className="list-item" >
-              <ListItemAvatar>
-                <Avatar className="avatar" >
-                  <FaceIcon />
-                </Avatar>
-
-              </ListItemAvatar>
-              <ListItemText>
-                Bai 26: Study with meomeo <br />
-              Bai 27: You will be successfull! <br />
-              Bai 28: Study with meomeo <br />
-              </ListItemText>
-            </ListItem>
+            {/* RESOURCES */}
+            <Typography variant="h6" style={{ textAlign: "center" }} >Nội dung</Typography>
+            {
+              Object.keys(resources).map((key) => {
+                const { active, value } = resources[key];
+                if (!active || value.length === 0) return null;
+                if (hideDivider === null) hideDivider = key;
+                return <React.Fragment key={key} >
+                  {hideDivider !== key && <Divider className='divider' />}
+                  <ListItem className="list-item" >
+                    <ListItemAvatar>
+                      <Avatar className="avatar" >{key !== "MY_VOCA" ? key : "Me"}</Avatar>
+                    </ListItemAvatar>
+                    <ListItemText>
+                      {value.map(el => {
+                        return <Typography key={el}
+                          className="lesson-name">
+                          {"Bai-" + el}
+                        </Typography>
+                      })}
+                    </ListItemText>
+                  </ListItem>
+                </React.Fragment>
+              })
+            }
           </List>
           <div className="edit-line" >
             <Button
