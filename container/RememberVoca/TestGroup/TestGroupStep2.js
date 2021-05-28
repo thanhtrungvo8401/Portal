99883099055262634a20fd21eaf6a58ef4563997
trung8401 <thanhtrungvo8401@gmail.com>
2021-05-28 07:40:17 +0700

    007 -- handle create layout for QA question

--- container/RememberVoca/TestGroup/TestGroupStep2.js
+++ container/RememberVoca/TestGroup/TestGroupStep2.js
@@ -1,5 +1,9 @@
+import { useSelector } from "react-redux";
 import TestGroupStep2UI from "./TestGroupStep2UI";
 
 export function TestGroupStep2() {
-  return <TestGroupStep2UI />
+  const { list } = useSelector(state => state.vocas);
+  const optionsMeaning = list.map(el => el.meaning).sort((a, b) => a > b ? 1 : -1);
+  const voca = { ...list[1], isIn: true }
+  return <TestGroupStep2UI optionsMeaning={optionsMeaning} voca={voca} />
 }
