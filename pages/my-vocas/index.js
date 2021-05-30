755e7aa177acbd81b43ec2c57db87c208d583628
trung8401 <thanhtrungvo8401@gmail.com>
2021-05-30 20:14:21 +0700

    007 -- handle refactor code

--- pages/my-vocas/index.js
+++ pages/my-vocas/index.js
@@ -1,45 +1,45 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
 import { withPrivateLayout } from "components/Layouts/PrivateLayout";
-import MyVocasLayout from "container/MySetVocas/Layout";
-import {
-  serviceCreateSetVoca,
-  serviceDeleteSetVocas,
-  serviceGetSetVocas,
-  serviceUpdateSetVoca,
-} from "service/setVocaService";
-import { storageKey } from "utils/Constant";
-import { localStorageHelper } from "utils/storageHelper";
+import TitlePage from "components/atoms/title-page";
+import { BodyContainer } from "components/atoms/body-wrapper";
+import React from "react";
+// import {
+//   serviceCreateSetVoca,
+//   serviceDeleteSetVocas,
+//   serviceGetSetVocas,
+//   serviceUpdateSetVoca,
+// } from "service/setVocaService";
+// import { storageKey } from "utils/Constant";
+// import { localStorageHelper } from "utils/storageHelper";
 function MySetVocas(props) {
   // Variables:
-  const { setVoca } = useSelector((state) => state.setVocas);
-  const dispatch = useDispatch();
-  const user = JSON.parse(localStorageHelper.get(storageKey.MY_PROFILE)) || {};
+  // const { setVoca } = useSelector((state) => state.setVocas);
+  // const dispatch = useDispatch();
+  // const user = JSON.parse(localStorageHelper.get(storageKey.MY_PROFILE)) || {};
 
-  const handleOnSubmitModal = () => {
-    if (!setVoca.id) {
-      dispatch(serviceCreateSetVoca({ ...setVoca, authorId: user.id }));
-    } else {
-      dispatch(serviceUpdateSetVoca(setVoca));
-    }
-  };
+  // const handleOnSubmitModal = () => {
+  //   if (!setVoca.id) {
+  //     dispatch(serviceCreateSetVoca({ ...setVoca, authorId: user.id }));
+  //   } else {
+  //     dispatch(serviceUpdateSetVoca(setVoca));
+  //   }
+  // };
 
-  const handleOnRemoveSetVocaById = (setVocaId) => {
-    dispatch(serviceDeleteSetVocas(setVocaId));
-  };
+  // const handleOnRemoveSetVocaById = (setVocaId) => {
+  //   dispatch(serviceDeleteSetVocas(setVocaId));
+  // };
 
-  useEffect(() => {
-    if (user.id) {
-      // fetch set-vocas by authorId:
-      dispatch(serviceGetSetVocas(user.id));
-    }
-  }, [user.id]);
-  return (
-    <MyVocasLayout
-      handleOnRemoveSetVocaById={handleOnRemoveSetVocaById}
-      handleOnSubmitModal={handleOnSubmitModal}
-    />
-  );
+  // useEffect(() => {
+  //   if (user.id) {
+  //     // fetch set-vocas by authorId:
+  //     dispatch(serviceGetSetVocas(user.id));
+  //   }
+  // }, [user.id]);
+  return <React.Fragment>
+    <TitlePage>Tạo nhóm từ vựng của riêng bạn</TitlePage>
+    <BodyContainer>Hello</BodyContainer>
+  </React.Fragment>
 }
 
 export default withPrivateLayout(MySetVocas, {
