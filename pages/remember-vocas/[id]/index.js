43b19a001e82d8152fa9ff006523d4fc0a5c72c9
trung8401 <thanhtrungvo8401@gmail.com>
2021-05-31 08:18:29 +0700

    007 -- handle refactor code

--- pages/remember-vocas/[id]/index.js
+++ pages/remember-vocas/[id]/index.js
@@ -1,29 +1,31 @@
-import { useRouter } from "next/router";
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { withPrivateLayout } from "components/Layouts/PrivateLayout";
-import StudyModal from "container/RememberVoca/StudyModal/StudyModal";
-import { serviceGetRememberById } from "service/rememberService";
-import { serviceGetVocasByCodes } from "service/vocaService";
-
+import TitlePage from "components/atoms/title-page";
+import { BodyContainer } from "components/atoms/body-wrapper";
+import Remember_Id_Instruction from "components/organisms/remember-vocas-[id]/instruction";
 function StudyRememberGroupById() {
-  const { id } = useRouter().query;
-  const dispatch = useDispatch();
-  const { rememberGroup } = useSelector((state) => state.rememberGroups);
+  // const { id } = useRouter().query;
+  // const dispatch = useDispatch();
+  // const { rememberGroup } = useSelector((state) => state.rememberGroups);
 
-  React.useEffect(() => {
-    if (id) {
-      dispatch(serviceGetRememberById(id));
-    }
-  }, [id]);
+  // React.useEffect(() => {
+  //   if (id) {
+  //     dispatch(serviceGetRememberById(id));
+  //   }
+  // }, [id]);
 
-  React.useEffect(() => {
-    if (rememberGroup && rememberGroup.vocaCodes) {
-      dispatch(serviceGetVocasByCodes(rememberGroup.vocaCodes));
-    }
-  }, [rememberGroup && rememberGroup.vocaCodes]);
+  // React.useEffect(() => {
+  //   if (rememberGroup && rememberGroup.vocaCodes) {
+  //     dispatch(serviceGetVocasByCodes(rememberGroup.vocaCodes));
+  //   }
+  // }, [rememberGroup && rememberGroup.vocaCodes]);
 
-  return <StudyModal />;
+  return <React.Fragment>
+    <TitlePage>Cố gắng nhớ tất cả từ vựng bên dưới</TitlePage>
+    <BodyContainer>
+      <Remember_Id_Instruction />
+    </BodyContainer>
+  </React.Fragment>
 }
 
 export default withPrivateLayout(StudyRememberGroupById, {
