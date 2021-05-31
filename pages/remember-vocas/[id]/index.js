23d0ad116d49f0b21223b1f1f57075d351d06426
trung8401 <thanhtrungvo8401@gmail.com>
2021-05-31 09:01:07 +0700

    007 -- handle refactor code

--- pages/remember-vocas/[id]/index.js
+++ pages/remember-vocas/[id]/index.js
@@ -1,24 +1,26 @@
 import React from "react";
+import { useRouter } from "next/router";
 import { withPrivateLayout } from "components/Layouts/PrivateLayout";
-import TitlePage from "components/atoms/title-page";
 import { BodyContainer } from "components/atoms/body-wrapper";
+import TitlePage from "components/atoms/title-page";
 import Remember_Id_Instruction from "components/organisms/remember-vocas-[id]/instruction";
+import { serviceGetRememberById } from "service/rememberService";
+import { serviceGetVocasByCodes } from "service/vocaService";
+import { useDispatch, useSelector } from "react-redux";
+
 function StudyRememberGroupById() {
-  // const { id } = useRouter().query;
-  // const dispatch = useDispatch();
-  // const { rememberGroup } = useSelector((state) => state.rememberGroups);
-
-  // React.useEffect(() => {
-  //   if (id) {
-  //     dispatch(serviceGetRememberById(id));
-  //   }
-  // }, [id]);
-
-  // React.useEffect(() => {
-  //   if (rememberGroup && rememberGroup.vocaCodes) {
-  //     dispatch(serviceGetVocasByCodes(rememberGroup.vocaCodes));
-  //   }
-  // }, [rememberGroup && rememberGroup.vocaCodes]);
+  const { id } = useRouter().query;
+  const dispatch = useDispatch();
+  const { rememberGroup } = useSelector((state) => state.rememberGroups);
+  // Get current remember-group-by-id:
+  React.useEffect(() => {
+    if (id) dispatch(serviceGetRememberById(id));
+  }, [id]);
+  // handle get vocabulary base on this remember-group:
+  React.useEffect(() => {
+    const isValidData = rememberGroup && rememberGroup.vocaCodes;
+    if (isValidData) dispatch(serviceGetVocasByCodes(rememberGroup.vocaCodes));
+  }, [rememberGroup && rememberGroup.vocaCodes]);
 
   return <React.Fragment>
     <TitlePage>Cố gắng nhớ tất cả từ vựng bên dưới</TitlePage>
