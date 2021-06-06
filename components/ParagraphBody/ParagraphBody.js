5da4a8fb2a45061fd497bee5656be1e1bec59782
trung8401 <thanhtrungvo8401@gmail.com>
2021-06-06 14:43:44 +0700

    007 -- handle refactor code

--- components/ParagraphBody/ParagraphBody.js
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@material-ui/core";
-
-export default function ParagraphBody({ children, hidden }) {
-  return (
-    <Typography hidden={hidden} color="textSecondary" variant="body1">
-      {children}
-    </Typography>
-  );
-}
--- container/TestVocas/Layout.js
+++ container/TestVocas/Layout.js
@@ -1,21 +1,8 @@
-import { Container } from "@material-ui/core";
-import PageTitle from "../../components/PageComponent/PageTitle";
-import ParagraphTitle from "../../components/ParagraphTitle";
-import ParagraphBody from "../../components/ParagraphBody/ParagraphBody";
 import TestVocaBox from "../../components/TestVocaBox/TestVocaBox";
 
 function TestVocasLayout(props) {
   return (
     <div className="remember-vocas-layout">
-      <PageTitle>Let's create and check your memories</PageTitle>
-      <ParagraphTitle>Instruction</ParagraphTitle>
-      <Container>
-        <ParagraphBody>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-        </ParagraphBody>
-      </Container>
-      <ParagraphTitle>Learning vocabularies group</ParagraphTitle>
-      {/* TEST VOCA BOX */}
       <TestVocaBox />
     </div>
   );
