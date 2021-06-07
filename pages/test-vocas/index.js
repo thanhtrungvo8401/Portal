8662e1aef8be5979e5e05d26b2369bc6d6c1f5a1
trung8401 <thanhtrungvo8401@gmail.com>
2021-06-07 09:58:42 +0700

    handle refactor code

--- pages/test-vocas/index.js
+++ pages/test-vocas/index.js
@@ -4,6 +4,7 @@ import { withPrivateLayout } from "components/templates/main";
 import TitlePage from "components/atoms/title-page";
 import TestVocasInstruction from "components/organisms/test-vocas/instruction";
 import TestVocasBody from "components/organisms/test-vocas/body";
+import TestVocaModal from "components/organisms/test-vocas/update-modal";
 
 function TestVocas(props) {
   return <React.Fragment>
@@ -12,6 +13,7 @@ function TestVocas(props) {
       <TestVocasInstruction />
       <TestVocasBody />
     </BodyContainer>
+    <TestVocaModal />
   </React.Fragment>
 }
 
